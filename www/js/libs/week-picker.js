1acd9fc671ca63dcd5aad1a53bc16c63d0d90c2c
Bárta Michal - Allrisk <barta.michal@allrisk.cz>
2016-06-28 15:36:11 +0200

    week interval picker

--- www/js/libs/week-picker.js
+++ www/js/libs/week-picker.js
@@ -23,6 +23,48 @@ Date.prototype.nowMySQLdate = function() {
 		   ('0' + date.getDate()).slice(-2);
 }
 
+Date.prototype.getMondayOfWeek = function(w, y) {
+    var simple = new Date(y, 0, 1 + (w - 1) * 7);
+    var dow = simple.getDay();
+    var ISOweekStart = simple;
+    if (dow <= 4)
+        ISOweekStart.setDate(simple.getDate() - simple.getDay() + 1);
+    else
+        ISOweekStart.setDate(simple.getDate() + 8 - simple.getDay());
+    return ISOweekStart;
+}
+
+Date.prototype.getSundayOfWeek = function(w, y) {
+    var simple = new Date(y, 0, 1 + (w - 1) * 7);
+    var dow = simple.getDay();
+    var ISOweekStart = simple;
+    if (dow <= 4)
+        ISOweekStart.setDate(simple.getDate() - simple.getDay() + 1 + 6);
+    else
+        ISOweekStart.setDate(simple.getDate() + 8 - simple.getDay() + 6);
+    return ISOweekStart;
+}
+
+Date.prototype.convertDateToMySQLdate = function(date) {
+    return date.getFullYear()  + '-' +
+           ('0' + (date.getMonth()+1)).slice(-2) + "-" +
+           ('0' + date.getDate()).slice(-2);
+}
+
+Date.prototype.getMonday = function(date_string) {
+    var d = new Date(date_string);
+    var day = d.getDay(),
+        diff = d.getDate() - day + (day == 0 ? -6:1); // adjust when day is sunday
+    return new Date(d.setDate(diff));
+}
+
+Date.prototype.getSunday = function(date_string) {
+    var d = new Date(date_string);
+    var day = d.getDay(),
+        diff = d.getDate() - day + (day == 0 ? -6:1) + 6; // adjust when day is sunday
+    return new Date(d.setDate(diff));
+}
+
 function bindWeekPicker(input) {
     $(input).datepicker({
         todayBtn: "linked",
