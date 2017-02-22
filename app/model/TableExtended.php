c9adeea1cc6f1c822a275df79b2a911f02d171a5
Bárta Michal - Allrisk <barta.michal@allrisk.cz>
2017-02-22 16:54:45 +0100

    skript pro zálohy databáze

--- app/model/TableExtended.php
+++ app/model/TableExtended.php
@@ -5,7 +5,6 @@ use Nette;
 use Tracy\Debugger;
 
 abstract class TableExtended extends Table  { 
-
     public function insert($data)	{
         try {
             $columns = $this->connection->getStructure()
@@ -57,6 +56,63 @@ abstract class TableExtended extends Table  {
         }
         
     }    
+
+    public function backupAll() {
+        $columns = $this->connection->getStructure()
+                                    ->getColumns($this->tableName);
+        $rows = $this->getTable();
+
+        if($rows->count() == 0) 
+            return "";
+
+        $backup_string = "INSERT INTO `".$this->tableName."` (";
+        $column_types = [];
+
+        foreach($columns as $column) {
+            $backup_string .= "`".$column['name']."`, ";
+            $column_types[] = $column['nativetype'];
+        }                         
+
+        $backup_string = substr($backup_string, 0, -2);
+        $backup_string .= ") VALUES\r\n";
+
+        foreach ($rows as $row) {
+            $backup_string .= "(";
+            $col_index = 0;            
+            
+            foreach($row as $column_value) {
+                if($column_types[$col_index] == "VARCHAR" || $column_types[$col_index] == "CHAR" || $column_types[$col_index] == "ENUM")
+                    $column_value = "'".$this->escape($column_value)."'";
+                elseif($column_value == "")
+                    $column_value = "NULL";
+
+                $backup_string .= $column_value.", ";
+                $col_index++;
+            }
+            
+            $backup_string = substr($backup_string, 0, -2);
+            $backup_string .= "),\r\n";
+        }
+
+        $backup_string = substr($backup_string, 0, -3);
+        $backup_string .= ";";
+
+        return $backup_string;
+    }
+
+    
+    private function escape($value) {
+        $return = '';
+        for($i = 0; $i < strlen($value); ++$i) {
+            $char = $value[$i];
+            $ord = ord($char);
+            if($char !== "'" && $char !== "\"" && $char !== '\\')
+                $return .= $char;
+            else
+                $return .= '\\' . $char;
+        }
+        return $return;
+    }
 }
 
 class DuplicateException extends \Exception {
---
+++ app/presenters/BackupPresenter.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Presenters;
+
+use Nette;
+use App\Model;
+use Tracy\Debugger;
+use Nette\Application\UI\Form;
+
+class BackupPresenter extends Nette\Application\UI\Presenter {
+	
+	public function actionBackupDb() {
+		$backup_file = DB_BACKUP_FOLDER."/".date('Y-m-d--h-i-s').".sql";
+
+		$this->writeTableToBackupFile("country", $backup_file);
+		$this->writeTableToBackupFile("center", $backup_file);
+		$this->writeTableToBackupFile("category", $backup_file);
+		$this->writeTableToBackupFile("person", $backup_file);
+		$this->writeTableToBackupFile("book", $backup_file);
+		$this->writeTableToBackupFile("distribution", $backup_file);
+		$this->writeTableToBackupFile("book_priority", $backup_file);
+		$this->writeTableToBackupFile("show_center", $backup_file);
+
+		$this->payload->success = 1;
+		$this->sendPayload();
+	}
+
+	private function writeTableToBackupFile($table, $backup_file) {
+		$persons_head = "";
+
+		if(file_exists($backup_file)) {
+			$persons_head .= "\r\n\r\n";
+		}
+
+		$persons_head .= "-- data pro tabulku `".$table."` \r\n\r\n";
+		$persons_backup = $this->context->getService($table)->backupAll();
+		file_put_contents($backup_file, $persons_head.$persons_backup, FILE_APPEND | LOCK_EX);		
+	}
+}
